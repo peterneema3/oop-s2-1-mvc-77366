2eec85fd4761051deb2657401d214e67f6952112
peterneema3 <PeterNeema@PC-G84784S4>
2026-03-18 19:23:16 +0000

    Created controllers

---
+++ Library.MVC/Controllers/BooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.MVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Library.MVC/Controllers/LoansController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.MVC.Controllers
+{
+    public class LoansController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
